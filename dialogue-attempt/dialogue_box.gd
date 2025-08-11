3a60ba5c28ea9b3165f445f6283850de2e3f3c49
Eric Nguyen <ericnguyen455@gmail.com>
2025-08-11 16:38:54 -0400

    display consecutive dialogue

--- dialogue-attempt/dialogue_box.gd
+++ dialogue-attempt/dialogue_box.gd
@@ -4,11 +4,20 @@ class_name DialogueBox
 @onready var label: Label = $MarginContainer/Label
 
 var delay: bool  = true
+var is_displaying: bool = false
+
+var dialogue_one: String = "You spoke with such ferocity the other day that I thought you would have our heads, your Majesty."
+var dialogue_two: String = "Allow me to apologize on behalf of the Order. It was entirely my fault that things turned out this way."
+var dialogue_three: String = "So please, if anything..."
+
+var dialogue_array: Array = [dialogue_one, dialogue_two, dialogue_three]
+
+var dialogue_index: int = 0
 
 func _ready() -> void:
 	gui_input.connect(_on_gui_input)
 	
-	display_text("Her Highness is bright and generous. This subject has been treated with care and more each day.")
+	display_text(dialogue_array[dialogue_index])
 
 func _process(_delta: float) -> void:
 	pass
@@ -16,6 +25,9 @@ func _process(_delta: float) -> void:
 func display_text(text: String):
 	# Clear the text first
 	label.text = ""
+	
+	# Set the displaying bool to true
+	is_displaying = true
 	
 	# Stagger the display of the text
 	for char in text:
@@ -27,9 +39,27 @@ func display_text(text: String):
 	
 	# Reset the delay mode to true
 	delay = true
+	
+	# Reset the displaying boo
+	is_displaying = false
+
+func close_dialogue_box() -> void:
+	$".".hide()
 
 func _on_gui_input(event: InputEvent):
-	# If the DialogueBox is clicked, "skip" the text delay by toggling
-	# the delay to false
+	
 	if event.is_action_pressed("left_click"):
-		delay = false
+		# Close the dialogue box if this is the last line in the current dialogue array
+		if dialogue_index == dialogue_array.size() - 1:
+			close_dialogue_box()
+			return
+			
+		# If the DialogueBox is clicked while text is being displayed, "skip" the text delay by toggling
+		# the delay to false
+		if is_displaying:
+			delay = false
+		# If the DialogueBox is clicked after the text has finished displaying, proceed to the next
+		# dialogue line
+		elif !is_displaying:
+			dialogue_index = mini(dialogue_index + 1, dialogue_array.size() - 1)
+			display_text(dialogue_array[dialogue_index])
